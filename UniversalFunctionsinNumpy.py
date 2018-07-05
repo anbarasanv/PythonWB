865003fa66e36e4a237edd261921d31e83e98b68
anbarasanv <33603902+anbarasanv@users.noreply.github.com>
2018-07-05 23:27:34 +0530

    Useful Universal Functions in Numpy Functions

---
+++ UniversalFunctionsinNumpy.py
@@ -0,0 +1,16 @@
+#Universal Functions in Numpy
+import numpy as np 
+B = np.arange(3)
+print(np.exp(B))
+print(np.sin(B))
+print(np.cos(B))
+C = np.array([0,1,2])
+print(np.add(B,C))
+'''
+Bellow Functions will be usfull
+all, any, apply_along_axis, argmax, argmin, argsort, average, bincount, ceil, clip, conj,
+corrcoef, cov, cross, cumprod, cumsum, diff, dot, floor, inner, inv, lexsort, max, maximum,
+mean, median, min, minimum, nonzero, outer, prod, re, round, sort, std, sum, trace, transpose,
+var, vdot, vectorize, where
+
+'''
