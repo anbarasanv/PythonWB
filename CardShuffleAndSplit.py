e2a0ca963facfbfc115c6534ab005470a79140db
anbarasanv <33603902+anbarasanv@users.noreply.github.com>
2018-09-08 13:25:15 +0530

    upgraded and commented

--- CardShuffleAndSplit.py
+++ CardShuffleAndSplit.py
@@ -1,12 +1,13 @@
 import itertools
 import random as rand
-n = list(itertools.product(range(2,11),['Spade','Heart','Diamond','Club']))+list(itertools.product(['A','J','Q','K'],['Spade','Heart','Diamond','Club']))
-rand.shuffle(n)
-p1 = []
-p2 = []
-p3 = []
-p4 = []
-for item in n:
+#Below line creates a card deck.
+deck = list(itertools.product(range(2,11),['Spade','Heart','Diamond','Club']))+list(itertools.product(['A','J','Q','K'],['Spade','Heart','Diamond','Club']))
+#The below line Shuffles the card deck.
+rand.shuffle(deck)
+#The below line creating four players.
+p1,p2,p3,p4 = [],[],[],[]
+#from the below loop we are spliting the card among 4.
+for item in deck:
     if(len(p1)<13):
         p1.append(item)
     elif(len(p2)<13):
