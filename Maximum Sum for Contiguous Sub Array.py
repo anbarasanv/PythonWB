da8f1358b1eb11b3ea05a2dce65f39a2cbdcfc95
anbarasanv <33603902+anbarasanv@users.noreply.github.com>
2018-09-25 12:41:58 +0530

    Timer Function has been added

--- Maximum Sum for Contiguous Sub Array.py
+++ Maximum Sum for Contiguous Sub Array.py
@@ -1,5 +1,8 @@
 #Time complexity: O(n)
 import sys
+import timeit
+#Timer has been started
+start = timeit.default_timer()
 def maxSumSubarray(arr):
     '''This function returns the maximum sum of contiguous sub array'''
     #Here finalMax declared to hold both positive and negative integers.
@@ -16,3 +19,6 @@ def maxSumSubarray(arr):
 testArray = [-13, -3, -25, -20, -3, -16, -23, -12, -5, -22, -15, -4, -7]
 #testArray = [-2, -3, 4, -1, -2, 1, 5, -3]
 print(maxSumSubarray(testArray))
+#Timer has been stoped
+stop = timeit.default_timer()
+print('Total time taken: ',start-stop)
